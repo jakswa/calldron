6a4e9ed5b6691c38877959a44c64b806e431a964
Jake Swanson <jakswa@gmail.com>
2019-02-13 22:17:44 -0500

    fix ordering

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @calls = Call.order(created_at: :desc).last(10)
-    @messages = Message.order(created_at: :desc).last(20)
+    @calls = Call.order(created_at: :desc).take(5)
+    @messages = Message.order(created_at: :desc).take(5)
   end
 end
